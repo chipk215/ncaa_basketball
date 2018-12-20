07a8379d24baf546660719f13f720023025d4e9a
CHIPK <chipk215@msn.com>
2018-12-20 15:23:49 -0700

    Processing Massey Ratings.

--- WebScrape/main.py
+++ WebScrape/main.py
@@ -1,5 +1,101 @@
 from bs4 import BeautifulSoup
 import requests
+from io import StringIO
+import pandas as pd
+import os
+from pathlib import Path
+
+
+url_dict = {
+    2018: "https://www.masseyratings.com/cb/arch/compare2018-18.htm",
+    2017: "https://www.masseyratings.com/cb/arch/compare2017-18.htm",
+    2016: "https://www.masseyratings.com/cb/arch/compare2016-18.htm",
+    2015: "https://www.masseyratings.com/cb/arch/compare2015-18.htm",
+    2014: "https://www.masseyratings.com/cb/arch/compare2014-19.htm",
+    2013: "https://www.masseyratings.com/cb/arch/compare2013-19.htm",
+    2012: "https://www.masseyratings.com/cb/arch/compare2012-18.htm",
+    2011: "https://www.masseyratings.com/cb/arch/compare2011-18.htm",
+    2010: "https://www.masseyratings.com/cb/arch/compare2010-18.htm",
+    2009: "https://www.masseyratings.com/cb/arch/compare2009-18.htm",
+    2008: "https://www.masseyratings.com/cb/arch/compare2008-18.htm",
+    2007: "https://www.masseyratings.com/cb/arch/compare2007-18.htm",
+    2006: "https://www.masseyratings.com/cb/arch/compare2006-18.htm",
+    2005: "https://www.masseyratings.com/cb/arch/compare2005-18.htm",
+    2004: "https://www.masseyratings.com/cb/arch/compare2004-17.htm",
+    2003: "https://www.masseyratings.com/cb/arch/compare2003-14.htm"
+}
+
+width_dict = {
+    2018: [3, 4, 4, 4, 4,
+           6, 4, 4, 4, 4,
+           5, 18, 4, 7, 6,
+           4, 4, 4, 4, 6,
+           4, 4, 4, 4, 6,
+           4, 4, 4, 4, 5,
+           11, 4, 4, 4, 4,
+           4, 6, 4, 4, 4,
+           4, 6, 4, 4, 4,
+           4, 5, 11, 4, 4,
+           4, 4, 4, 6, 4,
+           4, 4, 4, 6, 4,
+           4, 4, 4, 5, 11,
+           4, 4, 4, 4, 4,
+           6, 4, 4, 4, 4,
+           6, 4, 4, 8, 7,
+           7],
+    2017: [3, 4, 4, 4, 4,
+           6, 4, 4, 4, 4,
+           5, 18, 4, 7, 6,
+           4, 4, 4, 4, 6,
+           4, 4, 4, 4, 6,
+           4, 4, 4, 4, 5,
+           11, 4, 4, 4, 4,
+           4, 6, 4, 4, 4,
+           4, 6, 4, 4, 4,
+           4, 5, 11, 4, 4,
+           4, 4, 4, 6, 4,
+           4, 4, 4, 6, 4,
+           4, 4, 4, 5, 11,
+           5, 4, 4, 4, 4,
+           6, 4, 4, 4, 4,
+           6, 4, 4, 4, 4,
+           4, 11, 4, 4, 4,
+           4, 4, 8, 7, 7],
+    2016: [3, 4, 4, 4, 4,
+           6, 4, 4, 4, 4,
+           5, 18, 4, 7, 6,
+           4, 4, 4, 4, 6,
+           4, 4, 4, 4, 6,
+           4, 4, 4, 4, 5,
+           11, 4, 4, 4, 4,
+           4, 6, 4, 4, 4,
+           4, 6, 4, 4, 4,
+           4, 5, 11, 4, 4,
+           4, 4, 4, 6, 4,
+           4, 4, 4, 6, 4,
+           4, 4, 4, 5, 11,
+           5, 4, 4, 4, 4,
+           6, 4, 8, 7, 7],
+    2015: [],
+    2014: [],
+    2013: [],
+    2012: [],
+    2011: [],
+    2010: [],
+    2009: [],
+    2008: [],
+    2007: [],
+    2006: [],
+    2005: [],
+    2004: [],
+    2003: []
+}
+
+start_text_dict = {
+    2018: 'WLK BWE',
+    2017: 'BWE WLK',
+    2016: 'BWE FAS'
+}
 
 
 def make_csv_line(line):
@@ -11,13 +107,20 @@ def make_csv_line(line):
     return ','.join(stripped_words)
 
 
-def run_main():
+def compute_win_percentage(team_record):
+    components = team_record.split('-')
+    return float(int(components[0]))/(int(components[0]) + int(components[1]))
 
-    source = requests.get('https://www.masseyratings.com/cb/arch/compare2018-18.htm').text
+
+def run_main():
+    season = 2016
+
+    source = requests.get(url_dict[season]).text
     soup = BeautifulSoup(source, 'lxml')
+
     pre = soup.find('pre')
 
-    start_section_text = 'WLK BWE'
+    start_section_text = start_text_dict[season]
     end_section_text = '--------------------'
 
     page_text = pre.text.strip()
@@ -28,7 +131,7 @@ def run_main():
 
     lines = table_text.splitlines()
 
-    first_column_copied = False
+    first_row_copied = False
     result = []
     for line in lines:
         # handle blank lines
@@ -36,16 +139,57 @@ def run_main():
             # blank line do not copy into result
             continue
         elif line.lstrip(' ').startswith(start_section_text):
-            if not first_column_copied:
+            if not first_row_copied:
                 result.append(make_csv_line(line))
-                first_column_copied = True
+                first_row_copied = True
             else:
                 continue
         else:
             # not a blank line or a column header
-            result.append(make_csv_line(line))
+            result.append(line[1:])
 
     print(len(result))
+
+    # Rename all the occurrences of Rank and Team columns except for the first pair
+
+    # split the words in the line into a list
+    column_names = result[0].split(',')
+    rank_indices = [i for i, column in enumerate(column_names) if column == 'Rank']
+    rank_indices = rank_indices[1:]
+
+    for i in range(1, len(rank_indices)+1):
+        str_index = str(i)
+        column_names[rank_indices[i-1]] = 'Rank_' + str_index
+        column_names[rank_indices[i-1] + 1] = 'Team_' + str_index
+
+    string_buffer = ''
+    for line in result[1:]:
+        string_buffer = string_buffer + line + "\n"
+
+    # len(width_dict[season])
+    df = pd.read_fwf(StringIO(string_buffer), widths=width_dict[season], names=column_names,
+                     usecols=column_names[:75])
+
+    df['season'] = season
+
+    df['win_pct'] = df.apply(lambda row: compute_win_percentage(row.Record), axis=1)
+
+    drop_cols_2018 = ['Record', 'Rank_1', 'Team_1', 'Rank_2', 'Team_2', 'Rank_3', 'Team_3', 'BNT',
+                      'USA', 'AP', 'DES', 'Mean', 'Median', 'St.Dev']
+    drop_cols_2017 = ['Record', 'Rank_1', 'Team_1', 'Rank_2', 'Team_2', 'Rank_3', 'Team_3', 'Rank_4', 'Team_4',
+                      'USA', 'AP', 'DES', 'Mean', 'Median', 'St.Dev']
+
+    drop_cols_2016 = ['Record', 'Rank_1', 'Team_1', 'Rank_2', 'Team_2', 'Rank_3', 'Team_3', 'D1A',
+                      'USA', 'AP', 'DES', 'Mean', 'Median', 'St.Dev']
+
+    df.drop(columns=drop_cols_2016, inplace=True)
+
+    f_name = 'data/rankings_' + str(season) + '.csv'
+    csv_file = Path(f_name)
+    if csv_file.is_file():
+        os.remove(csv_file)
+
+    df.to_csv(csv_file, sep=',', index=False )
     return
 
 
