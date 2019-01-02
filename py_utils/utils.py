fa795fb98c376ca63ba3d84c3c5957e4bf7c3b3b
CHIPK <chipk215@msn.com>
2019-01-02 11:23:24 -0700

    Optimizing LR and Ada Boost

--- py_utils/utils.py
+++ py_utils/utils.py
@@ -1,4 +1,29 @@
 import operator
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import pandas as pd
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import log_loss
+from sklearn.model_selection import cross_val_score
+
+
+def display_confusion_matrix(y_test, y_pred):
+    cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+    class_names = [0, 1]
+    fig, ax = plt.subplots()
+    tick_marks = np.arange(len(class_names))
+    plt.xticks(tick_marks, class_names)
+    plt.yticks(tick_marks, class_names)
+    # create heatmap
+    sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap="YlGnBu", fmt='g')
+    ax.xaxis.set_label_position("top")
+    plt.tight_layout()
+    plt.title('Confusion matrix', y=1.1)
+    plt.ylabel('Actual label')
+    plt.xlabel('Predicted label')
+    return cnf_matrix
 
 
 # Compare the conferences of two teams playing in a game.
@@ -34,13 +59,13 @@ def join_feature_name_with_importance_value(features, importances):
     return feature_importances_sorted
 
 
-def display_important_features_rf_forest(classifier, features):
-    importances = classifier.feature_importances_
-    # print("Importances: ", importances)
-    feature_importances = join_feature_name_with_importance_value(features, importances)
-    print('Coefficient Values')
-    for items in feature_importances:
-        print(items[0], " ", items[1])
+def display_important_features(important_features, features, display=1):
+    feature_importances = join_feature_name_with_importance_value(features, important_features)
+    if display:
+        print('Coefficient Values')
+        for items in feature_importances:
+            print(items[0]," ", items[1])
+    return feature_importances
 
 
 def get_tournament_record(df, season, round_, team):
@@ -78,6 +103,7 @@ def print_game_info(df, season, round_, team):
         ('allow_ft_att_avg', False, float_format_string, 'Allow FT ATT Avg', False),
         ('to_avg', False, float_format_string, 'Turnover Avg', False),
         ('SRS', True, float_format_string, 'SRS', False),
+        ('SOS', True, float_format_string, 'SOS', False),
         ('pts_avg', True, float_format_string, 'PPG', False),
         ('poss_avg', True, float_format_string, 'Poss Avg', False),
         ('fg_pct', True, float_format_string, 'FG %', True),
@@ -91,14 +117,14 @@ def print_game_info(df, season, round_, team):
     ]
 
     # print the table header
-    print("{0:>45s}{1:>27s}{2:>15s}".format(team[0:25], opp_team[0:25], "Stat Upset"))
+    print("{0:>45s}{1:>27s}{2:>25s}".format(team[0:25], opp_team[0:25], "Stat Supports Winner"))
 
     for stat in stat_list:
         team_stat = stat[0] + '_t'
         opp_stat = stat[0] + '_o'
         stat_t = t_rec[team_stat].iloc[0]
         stat_o = t_rec[opp_stat].iloc[0]
-        upset_flag = stat_upset(stat[1], stat_t, stat_o, game_result)
+        support_flag = supporting_stat(stat[1], stat_t, stat_o, game_result)
 
         if stat[4]:
             stat_t = 100 * stat_t
@@ -108,14 +134,14 @@ def print_game_info(df, season, round_, team):
         if stat[1]:
             hint = "(H)"
 
-        print_string = stat[2].format(stat[3], stat_t, stat_o, str(upset_flag), hint)
+        print_string = stat[2].format(stat[3], stat_t, stat_o, str(support_flag), hint)
         # print(print_string)
         stat_count = stat_count + 1
-        if upset_flag:
+        if support_flag:
             upset_count = upset_count + 1
 
-        # save print data so i can be sorted
-        stat_dict[print_string] = upset_flag
+        # save print data so it can be sorted
+        stat_dict[print_string] = support_flag
 
     # sort list by upset flag (True or False)
     sorted_dict = sorted(stat_dict.items(), key=operator.itemgetter(1), reverse=True)
@@ -132,19 +158,20 @@ def print_game_info(df, season, round_, team):
     else:
         print('\n', opp_team, "Wins.")
 
-    print('\nUpset Stat Count =', upset_count, ' out of', stat_count, ' stats.')
-    return
+    print('\nSupporting Stat Count=', upset_count, ' out of', stat_count, ' stats.')
+    print('\n\n')
+    return sorted_dict
 
 
-def stat_upset(hi_stat_flag, stat_t, stat_o, game_result):
+def supporting_stat(hi_stat_flag, stat_t, stat_o, game_result):
     if hi_stat_flag:
-        upset_cond_1 = (game_result == 0) & (stat_t > stat_o)
-        upset_cond_2 = (game_result == 1) & (stat_t < stat_o)
+        cond_1 = (game_result == -1) & (stat_t > stat_o)
+        cond_2 = (game_result == 1) & (stat_t < stat_o)
     else:
-        upset_cond_1 = (game_result == 0) & (stat_t < stat_o)
-        upset_cond_2 = (game_result == 1) & (stat_t > stat_o)
+        cond_1 = (game_result == -1) & (stat_t < stat_o)
+        cond_2 = (game_result == 1) & (stat_t > stat_o)
 
-    return upset_cond_1 | upset_cond_2
+    return not(cond_1 | cond_2)
 
 
 def compute_percentage(x, tournament_games):
@@ -154,4 +181,43 @@ def compute_percentage(x, tournament_games):
     val = 100.0 * len(x) / game_count
 
     return val
+
+
+def get_hash_for_feature_labels(feature_list):
+    join_string = '-'.join(feature_list)
+    hash_value = hash(join_string)
+    return hash_value
+
+
+def save_model_stats(classifier, X, y, model_stats):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state= 5)
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    # save model stats
+    prediction_probabilities = classifier.predict_proba(X_test)
+    accuracy = metrics.accuracy_score(y_test, y_pred)
+    precision = metrics.precision_score(y_test, y_pred)
+    recall = metrics.recall_score(y_test, y_pred)
+    log_loss_value = log_loss(y_test, prediction_probabilities)
+    cross_val_scores = cross_val_score(classifier, X, y, cv=10, scoring='accuracy')
+    cross_validation_average = cross_val_scores.mean()
+    feature_labels = list(X_train)
+    hash_value = get_hash_for_feature_labels(feature_labels)
+    model_stats[hash_value] = {'accuracy': accuracy, 'precision': precision, 'recall': recall,
+                               'log_loss': log_loss_value, 'cross_validation': cross_validation_average,
+                               'labels': feature_labels}
+    return model_stats
+
+
+def negate_loser(x):
+    if x == 0:
+        x = -1
+    return x
+
+
+def hello():
+    print("Hello")
+
+
+
 
